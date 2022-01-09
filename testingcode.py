15864be2a0cc14f35dfa592b1041337a4fbc2260
Rodrigo Barria <tavinbarria@gmail.com>
2022-01-09 20:52:29 -0300

    commitv2

---
+++ testingcode.py
@@ -0,0 +1,19 @@
+#Test code to practice Git 
+
+#bla bla bla
+
+
+#cambios y más cambios, probando 123
+
+
+#more changes trying to keep my code online 
+
+
+# agregando nuevas cosas a repositorio, parte de commit 2
+
+
+# hago mas cambios en el código sin push, sólo guardando archivo para revisar versiones en Git Graph
+
+#se agrega cambio 3 para probar codigo
+
+#realizando cambios en v2 (commitv2)
